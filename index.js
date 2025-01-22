5d4c9df55889d1a8eade14389449024422509009
theresa897 <dzeufacknouboudemtheresa@gmail.com>
2025-01-22 17:42:18 +0100

    footer

---
+++ index.js
@@ -0,0 +1,25 @@
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const sections = document.querySelectorAll('div[id]');
+        const navItems = document.querySelectorAll('.nav-menu-item');
+
+        window.addEventListener('scroll', () => {
+            let current = '';
+
+            sections.forEach(section => {
+                const sectionTop = section.offsetTop;
+                const sectionHeight = section.clientHeight;
+
+                if (pageYOffset >= sectionTop - sectionHeight / 3) {
+                    current = section.getAttribute('id');
+                }
+            });
+
+            navItems.forEach(item => {
+                item.classList.remove('active');
+                if (item.getAttribute('href') === `#${current}`) {
+                    item.classList.add('active');
+                }
+            });
+        });
+    });
